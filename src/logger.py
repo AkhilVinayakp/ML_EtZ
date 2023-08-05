2eee89048e18b45dd67ef5034d6c2c1bcfe7a526
Akhil Vinayak P <nirvanaosborn@gmail.com>
2023-08-05 23:02:19 +0530

    test logger

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,12 @@
-import logger
+import datetime
+import logging
+import os
 
+LOG_DIR = os.path.join(os.getcwd(), "logs")
 class Mylogger:
     def __init__(self) -> None:
         '''
         initialize the logger functionalities
         '''
-        pass
+        # load the file name
+        self.file_name = f"{datetime.now().strftime('%d_%m_%Y')}.log"
