b8860af4752829bff5f55ef71cd3c425d8b2215d
Akhil Vinayak P <nirvanaosborn@gmail.com>
2023-08-13 12:55:46 +0530

    fix: logging
    logging function updated

--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,23 @@
-import datetime
+from datetime import datetime
 import logging
 import os
 
+
+
+
 LOG_DIR = os.path.join(os.getcwd(), "logs")
-class Mylogger:
-    def __init__(self) -> None:
-        '''
-        initialize the logger functionalities
-        '''
-        # load the file name
-        self.file_name = f"{datetime.now().strftime('%d_%m_%Y')}.log"
+file_name = f"{datetime.now().strftime('%d_%m_%Y')}.log"
+file_path = os.path.join(LOG_DIR, file_name)
+
+try:
+    os.makedirs(LOG_DIR,exist_ok=True)
+except Exception as e:
+    logging.error("Can not initiate folder path")
+logging.basicConfig(
+    filename=file_path,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+if __name__ == "__main__":
+    logging.info("logger initialized")
